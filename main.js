0b2370c927aa2eb5f8d7c6238bedeabaaabe8cf9
ASUS_ROG\MyPC <tk25042002@gmail.com>
2024-04-01 23:03:52 +0700

    code 2

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+let slideSlickers = document.querySelectorAll('.slick-slide');
+
+document.querySelector(".current_big_image").style.width = window.innerWidth + "px";
+window.addEventListener('resize', function () {
+    document.querySelector(".current_big_image").style.width = window.innerWidth + "px";
+});
+
 let locationNavigation = document.querySelectorAll('a[href="/tour_list.html"]')
         for (let i = 0; i < locationNavigation.length; i++) {
             locationNavigation[i].addEventListener('click', function (e) {
@@ -9,3 +16,29 @@ let locationNavigation = document.querySelectorAll('a[href="/tour_list.html"]')
             })
         }
     
+$(document).ready(function () {
+  $(".slick-track-2").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true,
+    draggable: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1500
+  });
+});
+
+    
+$(document).ready(function () {
+    $(".slick-track-3").slick({
+      slidesToShow: 10,
+      slidesToScroll: 2,
+      infinite: true,
+      arrows: true,
+      draggable: true,
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 1500
+    });
+  });
